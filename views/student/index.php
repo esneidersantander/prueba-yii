9c9c07a7b89b0154782ebc868602333f347a8158
LimberthEsneiderSantanderAlcivar <esneider_98@outlook.com>
2021-05-14 22:20:43 -0500

    Details in the views

--- views/student/index.php
+++ views/student/index.php
@@ -8,21 +8,36 @@ table th,td{
 }
 </style>
  
-<?= Html::a('Create', ['student/create'], ['class' => 'btn btn-success']); ?>
- 
-<table border="1">
-    <tr>
-        <th>Full_name</th>
-        <th>Address</th>
-        <th>Phone</th>
-        <th>Action</th>
-    </tr>
-    <?php foreach($model as $field){ ?>
-    <tr>
-        <td><?= $field->full_name; ?></td>
-        <td><?= $field->address; ?></td>
-        <td><?= $field->phone; ?></td>
-        <td><?= Html::a("Edit", ['student/edit', 'id' => $field->id]); ?> | <?= Html::a("Delete", ['student/delete', 'id' => $field->id]); ?> | <?= Html::a("View", ['student/view', 'id' => $field->id]); ?></td>
-    </tr>
-    <?php } ?>
-</table>
+
+
+<div class="site-index">
+    <div class="jumbotron">
+        <h1 style="color: #337ab7">CRUD APPLICATION</h1>
+    </div>
+    <?= Html::a('Create', ['student/create'], ['class' => 'btn btn-success']); ?>
+    <div class="body-content">
+        <div class="row">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>Full_name</th>
+                    <th>Address</th>
+                    <th>Phone</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($model as $field){ ?>
+                <tr class="table-active">
+                    <td><?= $field->full_name; ?></td>
+                    <td><?= $field->address; ?></td>
+                    <td><?= $field->phone; ?></td>
+                    <td><?= Html::a("Edit", ['student/edit', 'id' => $field->id], ['class'=>'label label-primary']); ?> | <?= Html::a("Delete", ['student/delete', 'id' => $field->id], ['class'=>'label label-danger']); ?> | <?= Html::a("View", ['student/view', 'id' => $field->id], ['class'=>'label label-default']); ?></td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+        </div>
+    </div>
+
+</div>
